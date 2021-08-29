f76bf528455150122faabd1ebb91e37165fa921e
Allan Cordeiro <allan.cordeiro.santos@gmail.com>
2021-08-29 13:01:47 -0300

    ajustes e arquivo de teste

--- atividade_conta.py
+++ atividade_conta.py
@@ -2,48 +2,46 @@
 # AC02 ADS-EaD - Implementação de classes
 # Allan Cordeiro
 # Email Impacta: allan.cordeiro@aluno.faculdadeimpacta.com.br
-import atividade_conta
-
 
 class Conta:
     def __init__(self, titular, agencia, numero, saldo_inicial):
-        self._titular = titular
-        self._agencia = agencia
-        self._numero = numero
-        self._saldo = saldo_inicial
-        self._ativa = False
-        self._operacoes = []
-        self._gravar_operacao('saldo inicial', self._saldo)
+        self.__titular = titular
+        self.__agencia = agencia
+        self.__numero = numero
+        self.__saldo = saldo_inicial
+        self.__ativa = False
+        self.__operacoes = []
+        self._gravar_operacao('saldo inicial', self.__saldo)
 
     # property blocks
     @property
     def titular(self):
-        return self._titular
+        return self.__titular
 
     @property
     def agencia(self):
-        return self._agencia
+        return self.__agencia
 
     @property
     def numero(self):
-        return self._numero
+        return self.__numero
 
     @property
     def saldo(self):
-        return self._saldo
+        return self.__saldo
 
     @property
     def ativa(self):
-        return self._ativa
+        return self.__ativa
 
     @ativa.setter
     def ativa(self, valor):
         if isinstance(valor, bool):
-            self._ativa = valor
+            self.__ativa = valor
 
     def depositar(self, valor):
         if self._validar_condicoes(valor):
-            self._saldo += valor
+            self.__saldo += valor
             self._gravar_operacao('deposito', valor)
             return True
         return False
@@ -51,26 +49,26 @@ class Conta:
     def sacar(self, valor):
         if self._validar_condicoes(valor):
             if self.saldo >= valor:
-                self._saldo -= valor
+                self.__saldo -= valor
                 self._gravar_operacao('saque', valor)
                 return True
         return False
 
     def transferir(self, conta_destino, valor):
         if conta_destino.ativa:
-            if self._validar_condicoes(valor) and self._saldo >= valor:
-                conta_destino._saldo += valor
-                self._saldo -= valor
+            if self._validar_condicoes(valor) and self.saldo >= valor:
+                conta_destino.__saldo += valor
+                self.__saldo -= valor
                 self._gravar_operacao('transferencia', valor)
-                conta_destino._gravar_operacao('transferencia', valor)
+                # conta_destino._gravar_operacao('transferencia', valor)
                 return True
         return False
 
     def tirar_extrato(self):
-        return self._operacoes
+        return self.__operacoes
 
     def _gravar_operacao(self, operacao, valor):
-        self._operacoes.append((operacao, valor))
+        self.__operacoes.append((operacao, valor))
 
     def _validar_condicoes(self, valor):
         if self.ativa and valor > 0:
--- teste.py
+++ teste.py
@@ -4,9 +4,11 @@ conta = atividade_conta.Conta('Allan', '123', '123', 50)
 conta_2 = atividade_conta.Conta('Jose', '1', '567', 0)
 conta.ativa = True
 conta_2.ativa = True
-print(conta.depositar(100))
-print(conta.transferir(conta_2, 100))
+print(f"Status da conta: {conta.ativa}")
+print(f"depositar {conta.depositar(100)}")
+print(conta.transferir(conta_2, 50))
 print(conta.tirar_extrato())
 print(conta.saldo)
 print(conta_2.saldo)
 print(conta_2.tirar_extrato())
+print(conta.tirar_extrato())
